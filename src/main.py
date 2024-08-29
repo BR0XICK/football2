3f1623b14f183c387447dc90b9a22b3d86c3ab6a
BR0XICK <kessingerbrock@gmail.com>
2024-08-29 14:22:20 -0400

    fixed mat saving after flake changes

--- src/main.py
+++ src/main.py
@@ -9,23 +9,22 @@ def get_position_players(pos: str, data: pd.DataFrame) -> pd.DataFrame:
     return data[data['position'] == pos]
 
 def save_csv_as_mat(csv: str):
-    csv_data = pd.read_csv('../player_stats_2023.csv')
+    csv_data = pd.read_csv('./data/player_stats_2023.csv')
     data = csv_data.drop(columns=unneeded_cols)
-    savemat('fantasy_stats.mat', data)
+    savemat('./data/fantasy_stats.mat', data)
 
 def load_mat() -> pd.DataFrame:
     try:
-        data = loadmat('./fantasy_stats.mat')
+        data = loadmat('./data/fantasy_stats.mat')
         return data
     except:
-        data = loadmat('./fantasy_stats.mat')
-        save_csv_as_mat('./player_stats_2023.csv')
+        save_csv_as_mat('./data/player_stats_2023.csv')
+        data = loadmat('./data/fantasy_stats.mat')
         return data
 
 def main() -> int:
-    #stats = load_mat()
-    #print(stats.keys())
-    print("Hello world")
+    stats = load_mat()
+    print(stats.keys())
     return 1
 
 if __name__ == "__main__":
